4535aed7008e114d9ff2985406661619ac4e94c9
Joaquín Román Colonnello <joaquincolonnello@hotmail.com>
2024-12-09 02:29:09 -0300

    Disable first pages of RAM to avoid null dereferences

--- .gdbinit
+++ .gdbinit
@@ -1,5 +1,6 @@
 file build/kernel8.elf
 break undef_func
+break *0x200
 target remote :1234
 tui layout split
 tui focus cmd
--- src/mmu.c
+++ src/mmu.c
@@ -18,7 +18,7 @@ typedef struct __attribute__((packed))
 typedef struct __attribute__((packed))
 {
     bool valid : 1;
-    unsigned format : 1;
+    unsigned : 1;
     unsigned attrIndx : 3;
     bool ns : 1;
     unsigned ap : 2;
@@ -38,11 +38,38 @@ typedef struct __attribute__((packed))
     int : 1;
 } MMU_Block;
 
+typedef struct __attribute__((packed))
+{
+    bool valid : 1;
+    unsigned res1 : 1;
+    unsigned attrIndx : 3;
+    bool ns : 1;
+    unsigned ap : 2;
+    unsigned sh : 2;
+    bool af : 1;
+    bool ng : 1;
+    unsigned long oa : 38;
+    bool gp : 1;
+    bool dbm : 1;
+    bool contiguous : 1;
+    bool pxn : 1;
+    bool uxn : 1;
+    int : 4;
+    unsigned pbha : 4;
+    int : 1;
+} MMU_Page;
+
 __attribute__ ((section (".bss.mmu")))
+__attribute__ ((aligned (0x1000)))
+MMU_Page
+mmuTable1n[0x200];
+__attribute__ ((section (".bss.mmu")))
+__attribute__ ((aligned (0x1000)))
 MMU_Block
 mmuTable1a[0x200],
 mmuTable1b[0x200];
 __attribute__ ((section (".bss.mmu")))
+__attribute__ ((aligned (64)))
 MMU_Table
 mmuTable0[2];
 
@@ -60,7 +87,7 @@ void *plainMap()
             .ap = 0b00,
             .attrIndx = 0,
             .af = true,
-            .contiguous = true,
+            .contiguous = i >= 16,
             .valid = true,
         };
     }
@@ -70,6 +97,13 @@ void *plainMap()
         mmuTable1a[i].attrIndx = 1;
         mmuTable1a[i].pxn = true;
     }
+    // Point first 2MB block to a special table to block null references
+    ((MMU_Table*)mmuTable1a)[0] = (MMU_Table)
+    {
+        .nextLevel = ((unsigned long)&mmuTable1n) >> 12,
+        .format = 1,
+        .valid = 1,
+    };
 
     // Setup second level 2 table for 4000 0000h - 4003 FFFFh
     // Device memory
@@ -91,6 +125,26 @@ void *plainMap()
     // Set all other entries to 0
     for(int i = 64; i < 0x200; i++)
         mmuTable1b[i] = (MMU_Block){0};
+
+    // Special table for 0h - 20 0000h
+    for(int i = 0; i < 0x200; i++)
+    {
+        mmuTable1n[i] = (MMU_Page)
+        {
+            .oa = ((1 << 12) * i) >> 12,
+            .sh = 0b11,
+            .uxn = true,
+            .ap = 0b00,
+            .attrIndx = 0,
+            .af = true,
+            .contiguous = true,
+            .valid = true,
+            .res1 = 1,
+        };
+    }
+    // First 16 entries = 64kb are invalidated
+    for(int i = 0; i < 16; i++)
+        mmuTable1n[i].valid = false;
 
     // Configure level 1 table pointing to the previous tables
     mmuTable0[0] = (MMU_Table)
